b4db1ac300152416e73de761d1ae0b844e5820ad
rkibet <ronkibet2012@gmail.com>
2020-03-02 15:33:24 +0300

    This contains Admin files for admin panel

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,4 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Jobweb</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    position: relative;
+  }
+  ul.nav-pills {
+    top: 20px;
+    position: fixed;
+  }
+  div.col-8 div {
+    height: 620px;
+  }
+  </style>
+</head>
+<body data-spy="scroll" data-target="#myScrollspy" data-offset="1">
 
 <div class="container-fluid">
   <div class="row">
@@ -241,4 +268,7 @@
     </div>
   </div>
 </div>
+
+</body>
+</html>
 
